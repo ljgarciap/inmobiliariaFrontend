c4904dd7e82bd84d00f71eb328894df9428a5801
Luis García <lujogarpin78@gmail.com>
2025-09-22 10:57:50 -0500

    Minimal mod
    Minimal mod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api',
   appName: 'Inmobiliaria App',
-  googleMapsApiKey: 'AIzaSyBsRe8YyNT8i3Brj-fzQZ-JFhxwzVrwC58'
+  googleMapsApiKey: 'Enviada al correo'
 };
